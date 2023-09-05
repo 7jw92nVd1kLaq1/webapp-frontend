0f43b20946e32931d417f47d24d28916d99b8997
asdf <asdf@asdfs-MacBook-Pro.local>
2023-09-05 17:35:09 +0900

    useCustomersOrders hook implemented initially

--- src/hooks/useCustomersOrders.jsx
+++ src/hooks/useCustomersOrders.jsx
@@ -1,3 +1,68 @@
-const useCustomersOrders = () => {};
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+
+import {
+  resetState,
+  setEntries,
+  setPageRange,
+} from "@/redux/ListCustomerOrdersSlice";
+
+const useCustomersOrders = () => {
+  const dispatch = useDispatch();
+  const username = useSelector((state) => state.userSession.username);
+  const access_token = useSelector((state) => state.userSession.access_token);
+  const pageNumber = useSelector((state) => state.customerRequests.number);
+  const pageRange = useSelector((state) => state.customerRequests.pageRange);
+  const customersOrders = useSelector(
+    (state) => state.customerRequests.entries
+  );
+
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState("");
+
+  const fetchCustomersOrders = async () => {
+    dispatch(resetState());
+    setIsLoading(true);
+
+    if (!username) {
+      setError("Please Log-in.");
+      setIsLoading(false);
+      return;
+    }
+
+    const fetchOptions = {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${access_token}`,
+      },
+    };
+    try {
+      const response = await fetch(
+        `http://127.0.0.1:8000/api/list-requests/?page=${pageNumber}`,
+        fetchOptions
+      );
+      const resp_json = await response.json();
+      dispatch(setPageRange(resp_json.page_range));
+      dispatch(setEntries(resp_json.results));
+    } catch (e) {
+      setError("There was an error while fetching orders. Please try again.");
+    }
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    fetchCustomersOrders();
+  }, [pageNumber]);
+
+  return {
+    pageNumber,
+    pageRange,
+    customersOrders,
+    fetchCustomersOrders,
+    isLoading,
+    error,
+  };
+};
 
 export default useCustomersOrders;
