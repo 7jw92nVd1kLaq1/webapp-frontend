8acd3cced993164ac9345db4672b9e0d23783c51
asdf <asdf@asdfs-MacBook-Pro.local>
2023-09-22 13:35:08 +0900

    A box for an entry of information of intermediary added

--- src/routes/orders/view/customer/ViewOrderAsCustomer.jsx
+++ src/routes/orders/view/customer/ViewOrderAsCustomer.jsx
@@ -65,7 +65,7 @@ export default function ViewOrderAsCustomer() {
       </div>
       <div className="xl:flex justify-between items-center py-10 xl:divide-x divide-slate-300">
         <div className="xl:w-3/4 xl:pr-10 divide-y divide-slate-300">
-          <div className="flex justify-center items-center pb-10">
+          <div className="flex justify-center items-center pb-16">
             <div className="w-20 h-20 rounded-full border border-black relative">
               <img
                 src={addIntermediary}
@@ -170,9 +170,74 @@ export default function ViewOrderAsCustomer() {
               </div>
             </div>
             <p className="mt-12 text-xl font-bold">Assign Intermediary</p>
-            <p className="mt-2">
+            <p className="mt-2 mb-20">
               It's time to choose an intermediary for your order!
             </p>
+          </div>
+          <div className="">
+            <p className="text-lg my-10 font-medium">Choose Intermediary</p>
+            <div className="flex items-start flex-wrap gap-4">
+              <div className="p-10 rounded-lg shadow-md w-96 bg-stone-100">
+                <div className="text-[28px]">
+                  <p className="font-bold">James</p>
+                  <p className="font-medium">Offered you</p>
+                  <p className="font-semibold">30% off!</p>
+                </div>
+                <div className="mt-14 text-[24px]">
+                  <p className="font-medium">You Pay</p>
+                  <p className="font-semibold">123123.00</p>
+                  <p className="font-semibold">USD!</p>
+                </div>
+                <div className="w-full mt-14 flex gap-3">
+                  <button className="block bg-green-200 py-5 px-auto grow rounded-lg">
+                    <p className="text-center font-medium">Message</p>
+                  </button>
+                  <button className="block bg-sky-200 py-5 px-auto grow rounded-lg">
+                    <p className="text-center font-medium">Select</p>
+                  </button>
+                </div>
+              </div>
+              <div className="p-10 rounded-lg shadow-md w-96 bg-stone-100">
+                <div className="text-[28px]">
+                  <p className="font-bold">Alexander</p>
+                  <p className="font-medium">Offered you</p>
+                  <p className="font-semibold">30% off!</p>
+                </div>
+                <div className="mt-14 text-[24px]">
+                  <p className="font-medium">You Pay</p>
+                  <p className="font-semibold">700.00</p>
+                  <p className="font-semibold">USD!</p>
+                </div>
+                <div className="w-full mt-14 flex gap-3">
+                  <button className="block bg-green-200 py-5 px-auto grow rounded-lg">
+                    <p className="text-center font-medium">Message</p>
+                  </button>
+                  <button className="block bg-sky-200 py-5 px-auto grow rounded-lg">
+                    <p className="text-center font-medium">Select</p>
+                  </button>
+                </div>
+              </div>
+              <div className="p-10 rounded-lg shadow-md w-96 bg-stone-100">
+                <div className="text-[28px]">
+                  <p className="font-bold">Alexander</p>
+                  <p className="font-medium">Offered you</p>
+                  <p className="font-semibold">30% off!</p>
+                </div>
+                <div className="mt-14 text-[24px]">
+                  <p className="font-medium">You Pay</p>
+                  <p className="font-semibold">123123.00</p>
+                  <p className="font-semibold">USD!</p>
+                </div>
+                <div className="w-full mt-14 flex gap-3">
+                  <button className="block bg-green-200 py-5 px-auto grow rounded-lg">
+                    <p className="text-center font-medium">Message</p>
+                  </button>
+                  <button className="block bg-sky-200 py-5 px-auto grow rounded-lg">
+                    <p className="text-center font-medium">Select</p>
+                  </button>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
         <div className="xl:w-1/4 pt-10 pl-10"></div>
